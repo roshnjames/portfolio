24b4be5fec41888661640785f4f574fb902f22bd
roshin <roshinjamesjohn009@gmail.com>
2024-11-20 02:24:17 +0530

    exp heading aligned for dektop

--- src/Experience/Experience.jsx
+++ src/Experience/Experience.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import '../Card/Card.css'
 import './Experience.css'
+
+
 const Experience = () => {
+    const heading="experience".split('')
+
   return (
+    <>
+
+    <div className='heading-banner'>
+        {heading.map((char,index)=>(
+            <span className='heading-text' key={index}>{char}</span>
+        ))}
+    </div>
+
+
     <div className='exp-main-div'>
         <div className='Card'>
             from the exp page
         </div>
-        <div className='Card'>
-            from the exp page
-        </div>
-        <div className='Card'>
-            from the exp page
-        </div>
-        <div className='Card'>
-            from the exp page
-        </div>
-        <div className='Card'>
-            from the exp page
-        </div>
+
     </div>
+    </>
   )
 }
 
