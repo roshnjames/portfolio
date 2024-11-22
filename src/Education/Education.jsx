5522db0efcc87f49929493076881cd0d26ec035c
roshin <roshinjamesjohn009@gmail.com>
2024-11-22 20:51:35 +0530

    edu timeline at center

--- src/Education/Education.jsx
+++ src/Education/Education.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 import './Education.css'
 
 const Education = () => {
+    const educations=[
+        
+        {
+            institution:'College of Engineering Trivandrum,(CET)',
+            course:'MCA',
+            year:2024,
+        },
+        {
+            institution:'Marian College, Kuttikkanam',
+            course:'BCA',
+            year:2022,
+        },
+        {
+            institution:'Jawahar Navodaya Vidyalaya, Kottayam',
+            course:'Twelfth',
+            year:2018
+        },
+        {
+            institution:'Jawahar Navodaya Vidyalaya, Kottayam',
+            course:'Tenth',
+            year:2016
+        },
+    ]
+
+
+
     const heading="education".split('')
+
+    
     return (
         <>
             <div className='heading-banner'>
@@ -12,7 +40,21 @@ const Education = () => {
             </div>
 
             <div className='edu-main-div'>
-            hi its roshin
+
+                {educations.map((edu,index)=>(
+
+                    <div className='edu-parent-holder' key={index} style={{left:`${(index+1)%2!==0?'0%':'48.5%'}`}}>
+                        <div className='edu-logo-holder' style={(index+1)%2!==0?{left:'90%'}:undefined}/>
+                        <div className='edu-content-holder' style={(index+1)%2!==0?{left:'0%'}:{left:'14%'}}>
+                            <b>{edu.institution}</b>
+                            <p>{edu.course}</p>
+                            <small>{edu.year}</small>
+                        </div>
+                    </div>
+
+                ))}
+
+                
             </div>
 
 
