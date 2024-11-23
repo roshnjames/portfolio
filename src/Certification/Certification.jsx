8a48a314b7c4975ba86a69d65f15419b51e343cf
roshin <roshinjamesjohn009@gmail.com>
2024-11-23 21:18:03 +0530

    cards in loop for certs

--- src/Certification/Certification.jsx
+++ src/Certification/Certification.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import './Certification.css';
-// image imports
+// wallpaper imports
 import java from '../assets/certification/wallpaper/java.png';
+
+// certificates imports
+import java_crash from '../assets/certification/certificates/java_crash.jpeg';
+import java_oop from '../assets/certification/certificates/java_oop.png';
+
+
 
 const Certification = () => {
   // Add certifications in this array
   const certifications=[
     {
-
+      course:'Java with object-oriented programmming',
+      issuer:'Udemy',
+      date:'Oct 24',
+      wallpaper:java,
+      doc:java_oop
+    },
+    {
+      course:'Java for programmers',
+      issuer:'Udemy',
+      date:'Sept 24',
+      wallpaper:java,
+      doc:java_crash
     }
   ]
 
@@ -26,24 +43,21 @@ const Certification = () => {
 
       <div className='certs-main-div'>
 
+        {certifications.map((cert,index)=>(
+
+            <div className='certs-card' key={index}>
+                <div className="certs-bgpic-section" style={{backgroundImage:`url(${cert.wallpaper})`}} />
+
+                <div className="certs-details-section">
+
+                </div>
+                <div className="certs-index">
+
+                </div>
+            </div>
+        ))}
 
 
-
-        <div className='certs-card'>
-          <div className="certs-bgpic-section" style={{backgroundImage:`url(${java})`}}>
-
-          </div>
-          <div className="certs-details-section">
-
-          </div>
-          <div className="certs-index">
-
-          </div>
-        </div>
-        
-
-
-        
       </div>
     </>
   );
