b05f9ffb3582d7299a9485931782a570438e4337
roshin <roshinjamesjohn009@gmail.com>
2024-11-24 01:51:14 +0530

    menubar bug upon popup display ~resolved

--- src/Popup/Popup.jsx
+++ src/Popup/Popup.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import '../App.css';
 import './Popup.css';
 const Popup = (props) => {
+
+  useEffect(()=>{
+    if (props.visible){
+      document.body.classList.add('popup-active');
+    }else {
+      document.body.classList.remove('popup-active');
+    }
+    return () => {
+      document.body.classList.remove('popup-active'); // Clean up on unmount
+    };
+
+  },[props.visible])
 
   return props.visible?(
     <div className="popup-main-div">
       <div className="popup-content">
         <button className='popup-close-btn' onClick={()=>props.closeFunc(false)} >X</button>
+        <span className='button-app-left' style={{display:'none'}} />
           <div className='popup-content-doc'>
           
             <img className='popup-image' src={props.doc} draggable={false} onContextMenu={(e)=>e.preventDefault()}/>
