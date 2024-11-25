c40ab8a177c9bbef19042769c19f38d30fe8b42c
roshin <roshinjamesjohn009@gmail.com>
2024-11-25 18:37:32 +0530

    fit-content for heading-text implemented

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -5,11 +5,32 @@ import './Project.css'
 const Project = () => {
 
 
+  const heading='projects'.split('')
+
 
   return (
-    <div>
-      hello from projects
+    <>
+
+    <div className='proj-heading-banner'>
+      {heading.map((char,index)=>(
+        <span className='proj-heading-text' key={index}>
+          {char}
+        </span>
+      ))}
     </div>
+
+
+
+    
+    <div className='proj-main-div'>
+      <div className="proj-card">
+        <div className="card-index">1</div>
+        <div className="proj-details-section">ehlloo o i ai ghkh skd fk  kfjslf i.s jp r k sl sjf</div>
+        <button className="proj-doc-button">link</button>
+      </div>
+      
+    </div>
+    </>
   )
 }
 
