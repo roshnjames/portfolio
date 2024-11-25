417d8a070e1ef4ee8e56bb46a197bc1a8fbb5bf4
roshin <roshinjamesjohn009@gmail.com>
2024-11-25 23:54:18 +0530

    proj cards with content rendered

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -4,6 +4,44 @@ import './Project.css'
 
 const Project = () => {
 
+  //add projects in the below array
+
+  const projects=[
+    {
+      title:'Lost & Found',
+      domain:'Web app',
+      backend:'Django',
+      frontend:'HTML5, CSS',
+      database:'PostgreSQL',
+      link:'https://github.com/roshnjames/lost_found_django',
+    },
+    {
+      title:'Currency Classification for Visually Impaired',
+      domain:'Machine Learning',
+      backend:'Python, CNN',
+      frontend:'Tkinter',
+      database:null,
+      link:'https://github.com/roshnjames/currency-classification--ML',
+    },
+    {
+      title:'Ticket Reservation System',
+      domain:'Web app',
+      backend:'PHP',
+      frontend:'HTML5, CSS',
+      database:'MySQL',
+      link:'https://github.com/roshnjames/ticket-reservation-system-PHP',
+    },
+    {
+      title:'Face Mask & Social Distancing Analyzer',
+      domain:'Machine Learning',
+      backend:'Python, CNN',
+      frontend:'Tkinter',
+      database:null,
+      link:'https://github.com/roshnjames/Face-Mask-detection-Social-Distancing-analayzer',
+    },
+  ]
+
+
 
   const heading='projects'.split('')
 
@@ -23,11 +61,26 @@ const Project = () => {
 
     
     <div className='proj-main-div'>
-      <div className="proj-card">
-        <div className="card-index">1</div>
-        <div className="proj-details-section">ehlloo o i ai ghkh skd fk  kfjslf i.s jp r k sl sjf</div>
-        <button className="proj-doc-button">link</button>
-      </div>
+
+      {projects.map((proj,index)=>(
+         <div className="proj-card" key={index}>
+              <div className="card-index">{index+1}</div>
+              <div className="proj-details-section">
+                  <ul>
+                    <li>{proj.title}</li>
+                    <li>{proj.domain}</li>
+                    {proj.backend?<li>{proj.backend}</li>:undefined}
+                    {proj.frontend?<li>{proj.frontend}</li>:undefined}
+                    {proj.database?<li>{proj.database}</li>:undefined}
+
+                  </ul>
+              </div>
+              {proj.link?
+                  <button className="proj-doc-button" onClick={()=>window.open(proj.link,"_blank")}>link</button>
+              :undefined}
+        </div>
+      ))}
+     
       
     </div>
     </>
