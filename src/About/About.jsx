8b04cbf25b7590758d51489ded5897bac30a9e9f
roshin <roshinjamesjohn009@gmail.com>
2024-11-26 17:34:49 +0530

    aboutme initialized

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import pic from '../assets/pictures/aboutme.png'
 import './About.css'
 const About = () => {
   return (
     <div>
-      hello
+      <div className='about-image-holder' style={{backgroundImage:`url(${pic})`}}/>
     </div>
   )
 }
