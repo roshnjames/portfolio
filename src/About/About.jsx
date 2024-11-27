67627ae37606faa5abbbecde514d480b692d0c65
roshin <roshinjamesjohn009@gmail.com>
2024-11-27 15:58:14 +0530

    mail copy implemented using execCommand(copy)

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -10,11 +10,50 @@ const About = () => {
     const mail='roshinjames19@gmail.com'
     const aboutme=about
 
+
+    //mail copying with support for all browsers
     const [copymail,setCopymail]=useState(false)
-    const copyMailid=()=>{
-        navigator.clipboard.writeText(mail)
-        setCopymail(true)
-    }
+    const copyMailid = () => {
+        if (navigator.clipboard && navigator.clipboard.writeText) {
+            navigator.clipboard.writeText(mail)
+                .then(() => {
+                    setCopymail(true);
+                })
+                .catch((_) => {
+                    fallbackCopyTextToClipboard(mail);
+                });
+        } else {
+            fallbackCopyTextToClipboard(mail);
+        }
+    };
+    
+    // Fallback function for unsupported browsers
+    const fallbackCopyTextToClipboard = (text) => {
+        const textArea = document.createElement("textarea");
+        textArea.value = text;
+    
+        // Hide the textarea
+        textArea.style.position = "fixed";
+        textArea.style.left = "-9999px";
+        textArea.style.opacity = "0";
+        document.body.appendChild(textArea);
+    
+        // Select the text and copy it
+        textArea.focus();
+        textArea.select();
+    
+        try {
+            const successful = document.execCommand("copy");
+            if (successful)
+                setCopymail(true);
+        } catch (_) {
+            alert("Failed to copy mail id...")
+        }
+        document.body.removeChild(textArea);
+    };
+    
+
+
 
   return (
     <div className='about-page'>
@@ -27,7 +66,7 @@ const About = () => {
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{aboutme}
             </div>
             <div className="about-mail-copy">
-                <span className='mail-title'>Contact me:</span>
+                <span className='mail-title'>Contact me</span>
                 <span className='mail-id'>{mail}</span>
                 <button className='copy-button' style={{backgroundImage:`url(${copymail?tick:copy})`}} onClick={()=>copyMailid()} />
             </div>
---
+++ src/About/about.js
@@ -0,0 +1,8 @@
+const aboutme = "Hi, I am a self-taught software developer with good hands-on experience with Python. " +
+"Recently, I completed a Master’s degree in Computer Applications (MCA) from the College of Engineering, Trivandrum. " +
+"I am passionate about creating impactful and efficient solutions, eager to embrace new challenges, expand " +
+"my knowledge, and contribute meaningfully to projects. As a fresher, I am actively seeking opportunities " +
+"to grow and make a positive impact in the tech world.";
+
+
+export default aboutme
